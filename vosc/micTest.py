54ea62ffba86d4bbc258439cefc761fdee379f27
Kiran Sreekumar <52482677+EvilChangIndia@users.noreply.github.com>
2026-04-25 18:03:23 +0200

    indian

--- vosc/micTest.py
+++ vosc/micTest.py
@@ -11,7 +11,7 @@ def callback(indata, frames, time, status):
 # Download a small Vosk English model first (from a browser on your PC and scp,
 # or directly on the Nano if you prefer). Example path:
 #   /home/nvidia/models/vosk-model-small-en-us-0.15
-model_path = "/home/kiran/models/vosk-model-small-en-us-0.15"
+model_path = "/home/kiran/models/vosk-model-small-en-in-0.4"
 
 model = vosk.Model(model_path)
 recognizer = vosk.KaldiRecognizer(model, 16000)
